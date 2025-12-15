5850fc0c46e159dfc4d79773e239526dc3c38188
DonatasNoreika <donoras@gmail.com>
2025-12-15 13:02:07 +0200

    +Sukurti index puslapį su autoserviso statistika

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Service, Car, Order, OrderLine
 
-# Create your views here.
+def index(request):
+    context = {
+        'num_services': Service.objects.count(),
+        'num_cars': Car.objects.count(),
+        'num_orders_done': Order.objects.filter(status='c').count(),
+    }
+    return render(request, template_name="index.html", context=context)
