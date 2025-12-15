58c1d82ce7117a1d4ad23fb4294b583f43d21409
DonatasNoreika <donoras@gmail.com>
2025-12-15 12:52:54 +0200

    +Sutvarkyti admin site, kad tvarkingai rodytų automobilius (būtų paieška ir filtrai), paslaugas, užsakymus (paslaugos su kiekiais būtų matomos užsakyme)

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Service, Car, Order, OrderLine
 
-# Register your models here.
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['owner', 'make', 'model', 'license_plate', 'vin_code']
+    list_filter = ['owner', 'make', 'model']
+    search_fields = ['license_plate', 'vin_code']
+
+class OrderLineInLine(admin.TabularInline):
+    model = OrderLine
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['car', 'date', 'status']
+    inlines = [OrderLineInLine]
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Car, CarAdmin)
+admin.site.register(Order, OrderAdmin)
